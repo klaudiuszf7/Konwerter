2cefc8bdac18b337efbe6ddebfd21e9c18468534
Klaudiusz Fabiszak <klafab3@st.amu.edu.pl>
2021-04-28 13:39:14 +0200

    Dodałęm plik README

---
+++ README.R
@@ -0,0 +1,19 @@
+Nazwa pakietu: konwerter
+
+Pakiet pozwala na zamienienie odleglosci podanej w milach na kilometry.
+Może przydac się nam podczas pobytu w krajach anglosaskich.
+Będziemy mieli możliwość sprawdzenia danej odleglosci, poprzez przeliczenie jej
+na jednostke bardziej nam znana.
+
+Jak zainstalować pakiet?
+1. Pobierz pliki z repozytorium -Konwerter
+2. Otworz projekt w RStudio.
+3. Wpisz komende install.packages(Konwerter)
+4. Po zainstalowaniu pakiet powinien byc gotowy do uzycia.
+
+
+Przyklad uzycia:
+
+  mil_na_km(10)
+
+  mil_na_km= 16
